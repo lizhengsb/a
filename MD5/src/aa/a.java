18d0acdbe1c60ad36e6e0ab9f71dff10e44688eb
lizhengsb <2539852339@qq.com>
2018-08-28 23:33:03 +0800

    模拟

--- MD5/src/aa/a.java
+++ MD5/src/aa/a.java
@@ -1,11 +1,11 @@
-package aa;
+﻿package aa;
 
 public class a {
 	public static void main(String[] args) {
 		MD5 d=new MD5();
 		String b="ddd";
 		byte[] a=b.getBytes();
-		//李政
+		//李政11
 		System.out.println(d.getMD5(a));
 	}
 }
